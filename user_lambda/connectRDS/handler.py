a2681dac202e5981966cd452d615b012428a5aaf
sunyeongchoi <sn0716@naver.com>
2020-11-11 22:16:07 +0900

    user_lambda

--- user_lambda/connectRDS/handler.py
+++ user_lambda/connectRDS/handler.py
@@ -1,4 +1,102 @@
-import json
+import pymysql
+
+#Configuration Values
+endpoint = 'jmtgr.cij2xpo0vglc.us-east-1.rds.amazonaws.com'
+username = 'multicampusjmtgr'
+password = 'JMtgr1117'
+database_name = 'jmtgrdb'
+
+#Connection
+connection = pymysql.connect(endpoint, user=username,
+                passwd=password, db=database_name, charset='utf8')
+
+#User Insert / Select List - snchoi
+def UserAPI(event, context):
+    operation = event.httpMethod
+    if operation == 'GET':
+        cursor = connection.cursor()
+        cursor.execute('SELECT * from USER')
+        rows = cursor.fetchall()
+        # for row in rows:
+        #     print("{0} {1} {2}".format(row[0], row[1], row[2]))
+        return rows    
+    elif operation == 'POST':
+        email = event['email']
+        interest = event['interest']
+        name = event['name']
+        phone_number = event['phone_number']
+        
+        cursor = connection.cursor()
+        query = "INSERT INTO USER VALUES ('{}','{}','{}','{}')".format(email, interest, name, phone_number)
+        print('query : ', query)
+        cursor.execute(query)
+        connection.commit()
+        return "성공"
+    
+
+# User Select One - 회원 존재 유무 확인 , User Update - 회원 수정 / 로그인 시 기존회원인 경우 update
+def UserOneAPI(event, context):
+    operation = event.httpMethod
+    email = event["queryStringParameters"]["email"]
+    if operation == 'GET':
+        cursor = connection.cursor()
+        cursor.execute('SELECT * from USER where email="{}"').format(email)
+        rows = cursor.fetchall()
+        # for row in rows:
+        #     print("{0} {1} {2}".format(row[0], row[1], row[2]))
+        return rows    
+    elif operation == 'PUT':
+        email = event['email']
+        interest = event['interest']
+        name = event['name']
+        phone_number = event['phone_number']
+        
+        cursor = connection.cursor()
+        query = "INSERT INTO USER VALUES ('{}','{}','{}','{}')".format(email, interest, name, phone_number)
+        print('query : ', query)
+        cursor.execute(query)
+        connection.commit()
+        return "성공"
+
+
+# User Select One - 회원 존재 유무 확인 , User Update - 회원 수정 / 로그인 시 기존회원인 경우 update
+def UserOneAPI(event, context):
+    operation = event.httpMethod
+    email = event["queryStringParameters"]["email"]
+    if operation == 'GET':
+        cursor = connection.cursor()
+        cursor.execute('SELECT * from USER where email="{}"').format(email)
+        rows = cursor.fetchall()
+        # for row in rows:
+        #     print("{0} {1} {2}".format(row[0], row[1], row[2]))
+        return rows    
+    elif operation == 'PUT':
+        email = event['email']
+        interest = event['interest']
+        name = event['name']
+        phone_number = event['phone_number']
+        
+        cursor = connection.cursor()
+        query = "UPDATE USER SET interest = '{}', name = '{}', phone_number = '{}' WHERE email='{}'".format(interest, name, phone_number, email)
+        print('query : ', query)
+        cursor.execute(query)
+        connection.commit()
+        return "성공"
+
+
+
+# def lambda_handler(event, context):
+#     email = event['email']
+#     interest = event['interest']
+#     name = event['name']
+#     phone_number = event['phone_number']
+    
+#     cursor = connection.cursor()
+#     query = "INSERT INTO USER VALUES ('{}','{}','{}','{}')".format(email, interest, name, phone_number)
+#     print('query : ', query)
+#     cursor.execute(query)
+#     connection.commit()
+#     return "성공"
 
 
 def hello(event, context):
@@ -13,12 +111,3 @@ def hello(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
